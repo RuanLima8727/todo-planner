e761a72abd5168175df52662e582634503a1699f
Ruan <devd8727@gmail.com>
2021-07-16 10:20:42 -0300

    Rotas de criação e atualização de tarefas criadas

--- BACKEND/src/controller/TaskController.js
+++ BACKEND/src/controller/TaskController.js
@@ -7,6 +7,19 @@ class TaskController {
             .then((response=>{return res.status(200).json(response)}))
             .catch((error=>{return res.status(500).json(error)}));
     }
+    
+    async update (req, res) {
+       await TaskModel.findByIdAndUpdate({'_id': req.params.id}, req.body, {new:true})
+        .then((response)=>{return res.status(200).json(response)})
+        .catch((error) => {return res.status(500).json(error)})
+    }
+
+    async all (req, res) {
+        await TaskModel.find({macaddress:{'$in':req.body.macaddress}})
+        .sort('when')
+        .then((response)=>{res.status(200).json(response)})
+        .catch((error)=>{res.status(500).json(error)})
+    }
 }
 
 module.exports = new TaskController()
--- BACKEND/src/middlewares/TaskValidation.js
+++ BACKEND/src/middlewares/TaskValidation.js
@@ -1,5 +1,5 @@
-
-
+const TaskModel = require('../model/TaskModel')
+const {isPast} = require('date-fns')
 
 const TaskValidation = async function(req, res, next) {
     const {macaddress, type, tittle, description, when} = req.body;
@@ -7,9 +7,55 @@ const TaskValidation = async function(req, res, next) {
     if(!macaddress) {
         return res.status(400).json({error:"McAdress está vazio"})
     }
+    else if (!type) {
+        return res.status(400).json({error:"Tipo (Type) está vazio"})
+    }
+    else if (!tittle) {
+        return res.status(404).json({error:"Titulo está vazio"})
+    }
+    else if (!description) {
+        return res.status(400).json({error:"A descrição está vazia"})
+    }
+    else if (!when) {
+        return res.status(400).json({error:"O when está vazio"})
+    }
+    else if (isPast(new Date(when))) {
+        return res.status(400).json({error:"A data escohlida está no passado"})
+    }
     else {
+        let exists;
+        //Os 2 problemas resolvidos
+        //1-Atualizar uma tarefa B para ter a mesma data que uma tarefa A e assim ter duas tarefas na mesma data
+        //2-Não permitir atualizações de uma tarefa pois o código bloqueia atualização para quem tem a mesma data
+
+        if (req.params.id) {
+            //IF que será executado quando for feito um update
+            //Vai verificar se alguma outra tarefa além da em questão 
+            //Possui a mesma data que está sendo mandada
+            //Na requisição
+             exists = await TaskModel.findOne(
+                {
+                '_id' : {'$ne' : req.params.id}, //Tirando eu, tem alguma tarefa nessa mesma data?
+                'when': {'$eq' : new Date(when)},
+                'macaddress':{'$in': macaddress}
+                })
+        }
+
+        else {
+             exists = await TaskModel.findOne(
+                {
+                'when': {'$eq' : new Date(when)},
+                'macaddress':{'$in': macaddress}
+                })
+        }
+
+        if (exists) {
+            return res.status(400).json({error:"Você ja possui uma tarefa para tal data"})
+        }
+
         next()
     }
+
 
 }
 
