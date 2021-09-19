687482e531cf1af21bcecc6b0f555471fd3a20c2
Ruan <devd8727@gmail.com>
2021-09-19 12:11:25 -0300

    readme adjust

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const TaskRoutes = require('./routes/TaskRoutes')
+
 const server = express()
 server.use(express.json())
 
 server.listen(5000, ()=> {
 })
 
-const TaskRoutes = require('./routes/TaskRoutes')
 server.use('/task', TaskRoutes)
